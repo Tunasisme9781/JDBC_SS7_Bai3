0caf8c0969e258b7fcaa5b4bea91038fd471096f
Tuấn Ngọc <ngoctuan186205@gmail.com>
2026-06-30 04:31:33 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,112 +1,113 @@
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int employeeCount;
+        int currentIndex = 0;
 
-        int choice;
-        int count = 0;
-        double salary;
-        double sum = 0;
-        double bonus = 0;
-        double min = 500_000_000;
-        double max = 0;
-
-        do {
-            System.out.println("\n===== MENU =====");
-            System.out.println("1. Nhập lương nhân viên");
-            System.out.println("2. Hiển thị thống kê");
-            System.out.println("3. Tổng tiền thưởng");
-            System.out.println("4. Thoát");
-            System.out.print("Lựa chọn: ");
-
-            choice = Integer.parseInt(sc.nextLine());
-
-            switch (choice) {
-
-                case 1:
-                    while (true) {
-
-                        System.out.print("Nhập lương (-1 để dừng): ");
-                        salary = Double.parseDouble(sc.nextLine());
-
-                        if (salary == -1) {
-                            break;
-                        }
-
-                        if (salary < 0 || salary > 500_000_000) {
-                            System.out.println("Lương không hợp lệ!");
-                            continue;
-                        }
-
-                        count++;
-                        sum += salary;
-
-                        if (salary > max)
-                            max = salary;
-
-                        if (salary < min)
-                            min = salary;
-
-                        // Phân loại thu nhập
-                        if (salary < 5_000_000)
-                            System.out.println("Thu nhập thấp");
-                        else if (salary <= 15_000_000)
-                            System.out.println("Thu nhập trung bình");
-                        else if (salary <= 50_000_000)
-                            System.out.println("Thu nhập khá");
-                        else
-                            System.out.println("Thu nhập cao");
-
-                        // Tính thưởng
-                        if (salary < 5_000_000)
-                            bonus += salary * 0.05;
-                        else if (salary <= 15_000_000)
-                            bonus += salary * 0.10;
-                        else if (salary <= 50_000_000)
-                            bonus += salary * 0.15;
-                        else if (salary <= 100_000_000)
-                            bonus += salary * 0.20;
-                        else
-                            bonus += salary * 0.25;
-                    }
+        System.out.println("Nhập số lượng nhân viên: ");
+        employeeCount = Integer.parseInt(sc.nextLine());
+        Float[] arr = new Float[employeeCount];
+        for(int i = 0; i < employeeCount; i++)
+        {
+            System.out.printf("Nhập lương nhân viên %d: ",currentIndex+1);
+            do {
+                try {
+                    arr[currentIndex] = Float.parseFloat(sc.nextLine());
+                    currentIndex++;
                     break;
 
+                }catch (NumberFormatException e){
+                    System.err.print("\nLương phải là số thực! Nhập lại: \n");
+                }
+            }while (true);
+
+        }
+        int choice = 0;
+
+        do {
+
+            currentIndex=0;
+            System.out.println("************ QUẢN LÝ LƯƠNG NHÂN VIÊN ************");
+            System.out.println("1. Xem danh sách lương");
+            System.out.println("2. Sắp xếp lương");
+            System.out.println("3. Tìm kiếm lương cụ thể");
+            System.out.println("4. Thống kê lương");
+            System.out.println("5. Thoát");
+            System.out.print("Nhập lựa chọn của bạn: ");
+
+            switch (choice = Integer.parseInt(sc.nextLine())) {
+                case 1:
+                    System.out.println("----- DANH SÁCH LƯƠNG -----");
+                    for(int i = 0; i < employeeCount; i++){
+                        System.out.printf("Lương của nhân viên %d là: %.1f \n", currentIndex+1,arr[currentIndex]);
+                        currentIndex++;
+                    }
+                    break;
                 case 2:
 
-                    if (count == 0) {
-                        System.out.println("Chưa có dữ liệu");
-                        break;
-                    }
-
-                    System.out.println("\n===== THỐNG KÊ =====");
-                    System.out.println("Số nhân viên: " + count);
-                    System.out.printf("Lương TB: %.2f\n", sum / count);
-                    System.out.printf("Lương cao nhất: %.2f\n", max);
-                    System.out.printf("Lương thấp nhất: %.2f\n", min);
-                    System.out.printf("Tổng lương: %.2f\n", sum);
-
+                    System.out.println("Lương sắp xếp theo tăng dần: ");
+                    Arrays.sort(arr);
+                    System.out.println(Arrays.toString(arr));
                     break;
 
                 case 3:
-
-                    if (count == 0) {
-                        System.out.println("Chưa có dữ liệu");
-                    } else {
-                        System.out.printf("Tổng tiền thưởng: %.2f\n", bonus);
+                    boolean found = false;
+                    int count =0;
+                    Float salary;
+                    System.out.println("Nhập số lương cần tìm: ");
+                    salary = Float.parseFloat(sc.nextLine());
+                    for (int i = 0; i < employeeCount; i++){
+                        if(arr[i].equals(salary))
+                        {count ++;
+                            break;
+                        }
                     }
+                    if(count >0)
+                        System.out.println("Tìm thấy lương của nhân viên!");
 
+                        else System.out.println("Linear Search: Không tìm thấy");
                     break;
 
                 case 4:
-                    System.out.println("Kết thúc chương trình.");
-                    System.exit(0);
+                    float sumSalary = 0;
+                    float avgSalary;
+                    float maxSalary;
+                    float minSalary;
+                    int totalEmployee = 0;
+                    Arrays.sort(arr);
 
-                default:
-                    System.out.println("Lựa chọn không hợp lệ.");
+                    for(int i =0 ; i< employeeCount;i++)
+                    {
+                        sumSalary += arr[currentIndex];
+                        currentIndex++;
+                    }
+                    maxSalary = arr[currentIndex-1];
+                    minSalary = arr[0];
+                    avgSalary = sumSalary/employeeCount;
+                    for(int i =0 ; i< employeeCount;i++)
+                    {
+                        if(arr[i] > avgSalary)
+                            totalEmployee++;
+                    }
+                    System.out.printf("Tổng lương: %.1f\n", sumSalary);
+                    System.out.printf("Lương trung bình: %f\n", avgSalary);
+                    System.out.printf("Lương cao nhất: %.1f\n", maxSalary);
+                    System.out.printf("Lương thấp nhất: %.1f\n", minSalary);
+                    System.out.printf("Số nhân viên có lương trên trung bình: %d\n", totalEmployee);
+                    break;
+
+                case 5:
+                    System.out.println("Kết thúc chương trình!");
+                    System.exit(0);
             }
 
-        } while (true);
+
+        }while (true);
+
     }
 }
