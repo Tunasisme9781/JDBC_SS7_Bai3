6c8fc2c446e44e4b544f785d1be166b1efccc2ab
Tuấn Ngọc <ngoctuan186205@gmail.com>
2026-07-01 11:26:18 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -5,99 +5,59 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int elementCount;
-        int currentIndex = 0;
+        int row, col;
+        int currentRow = 0;
+        int currentCol = 0;
 
-        System.out.print("Nhập số phần tử của mảng: ");
-        elementCount = Integer.parseInt(sc.nextLine());
+        System.out.print("Nhập số hàng: ");
+        row = Integer.parseInt(sc.nextLine());
 
-        int[] arr = new int[elementCount];
+        System.out.print("Nhập số cột: ");
+        col = Integer.parseInt(sc.nextLine());
 
-        for (int i = 0; i < elementCount; i++) {
-            System.out.printf("Nhập phần tử %d: ", currentIndex + 1);
+        int[][] arr = new int[row][col];
 
-            do {
-                try {
-                    arr[currentIndex] = Integer.parseInt(sc.nextLine());
-                    currentIndex++;
-                    break;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
 
-                } catch (NumberFormatException e) {
-                    System.err.print("Phần tử phải là số nguyên! Nhập lại: ");
+                System.out.printf("Nhập phần tử [%d][%d]: ", currentRow, currentCol);
+
+                do {
+                    try {
+                        arr[currentRow][currentCol] = Integer.parseInt(sc.nextLine());
+                        break;
+
+                    } catch (NumberFormatException e) {
+                        System.err.print("Phần tử phải là số nguyên! Nhập lại: ");
+                    }
+
+                } while (true);
+
+                currentCol++;
+            }
+            currentRow++;
+            currentCol = 0;
+        }
+
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+
+                if (arr[i][j] % 2 == 0) {
+                    sumEven += arr[i][j];
+                } else {
+                    sumOdd += arr[i][j];
                 }
 
-            } while (true);
-        }
-
-        // Selection Sort giảm dần
-        for (int i = 0; i < elementCount - 1; i++) {
-
-            int maxIndex = i;
-
-            for (int j = i + 1; j < elementCount; j++) {
-                if (arr[j] > arr[maxIndex]) {
-                    maxIndex = j;
-                }
-            }
-
-            int temp = arr[i];
-            arr[i] = arr[maxIndex];
-            arr[maxIndex] = temp;
-        }
-
-        System.out.println("\nMảng sau khi sắp xếp giảm dần:");
-
-        for (int i = 0; i < elementCount; i++) {
-            System.out.print(arr[i] + " ");
-        }
-
-        System.out.println();
-
-        int number;
-
-        System.out.print("\nNhập số cần tìm: ");
-        number = Integer.parseInt(sc.nextLine());
-
-        // Linear Search
-        boolean found = false;
-
-        for (int i = 0; i < elementCount; i++) {
-            if (arr[i] == number) {
-                System.out.println("Linear Search: Tìm thấy tại vị trí " + (i + 1));
-                found = true;
-                break;
             }
         }
 
-        if (!found) {
-            System.out.println("Linear Search: Không tìm thấy!");
-        }
+        System.out.println("\nKết quả:");
 
-        // Binary Search trên mảng giảm dần
-        int left = 0;
-        int right = elementCount - 1;
-        found = false;
-
-        while (left <= right) {
-
-            int mid = (left + right) / 2;
-
-            if (arr[mid] == number) {
-                System.out.println("Binary Search: Tìm thấy tại vị trí " + (mid + 1));
-                found = true;
-                break;
-            }
-
-            if (number > arr[mid]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        if (!found) {
-            System.out.println("Binary Search: Không tìm thấy!");
-        }
+        System.out.println("Tổng số chẵn: " + sumEven);
+        System.out.println("Tổng số lẻ: " + sumOdd);
 
     }
 }
