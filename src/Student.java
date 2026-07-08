52a817d016e52c849e01152bbe4d4470ac3a00ab
Tunasisme9781 <ngoctuan186205@gmail.com>
2026-07-08 11:03:55 +0700

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,8 @@
 class Student {
-    // Các thuộc tính cơ bản (từ Bài tập 1)
     String studentId;
     String studentName;
     int age;
 
-    // Bổ sung phương thức displayData() - Không truyền tham số
     void displayData() {
         System.out.println("=== Thông tin Sinh viên ===");
         System.out.println("Mã sinh viên: " + studentId);
